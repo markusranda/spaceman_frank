8f83b995fdbdc5fa6fbc0c858e2ca0c14345d624
markusranda <randamarkus.95@gmail.com>
2025-06-29 20:00:39 +0200

    Now we only load it once

--- index.js
+++ index.js
@@ -202,16 +202,20 @@ async function init() {
   runGame();
 }
 
-window.addEventListener("DOMContentLoaded", () => {
-  backgroundCanvas = createBackgroundCanvasElement();
-  canvas.onre;
-  setTimeout(() => {
-    ctx.fillStyle = "#111";
-    ctx.fillRect(0, 0, worldX, worldY);
-    drawBackground(ctx, backgroundCanvas);
-    drawStartGame(ctx, canvas);
-  }, 100);
+window.addEventListener(
+  "DOMContentLoaded",
+  () => {
+    backgroundCanvas = createBackgroundCanvasElement();
+    canvas.onre;
+    setTimeout(() => {
+      ctx.fillStyle = "#111";
+      ctx.fillRect(0, 0, worldX, worldY);
+      drawBackground(ctx, backgroundCanvas);
+      drawStartGame(ctx, canvas);
+    }, 100);
 
-  document.addEventListener("click", init, { once: true });
-  document.addEventListener("keydown", init, { once: true });
-});
+    document.addEventListener("click", init, { once: true });
+    document.addEventListener("keydown", init, { once: true });
+  },
+  { once: true }
+);

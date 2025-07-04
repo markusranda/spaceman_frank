82a1963649b237f337697483e1008ce9c32642d2
markusranda <randamarkus.95@gmail.com>
2025-07-04 16:00:30 +0200

    Fixed enemy hitbox

--- docs/src/enemy.js
+++ docs/src/enemy.js
@@ -13,6 +13,7 @@ export class Enemy {
   attackTimer = MAX_ATTACK_TIMER;
   attackRange = 600;
   dead = false;
+  debugCircle = null;
 
   constructor(galaxy, frank) {
     const { x, y } = this.getRandomEdgeSpawnCoords(galaxy);
@@ -22,49 +23,52 @@ export class Enemy {
 
     this.sprite = new PIXI.Sprite(sprites["enemy_1"]);
     this.sprite.name = "enemy_1";
-    this.sprite.x = x;
-    this.sprite.y = y;
     this.sprite.width = this.radius * 2;
     this.sprite.height = this.radius * 2;
+
+    // Center sprite around enemy
+    this.sprite.anchor.set(0.5);
+    this.sprite.x = x;
+    this.sprite.y = y;
   }
 
   addTo(container) {
     if (!this.sprite.added) {
       container.addChild(this.sprite);
-      this.sprite.x = this.x - this.radius;
-      this.sprite.y = this.y - this.radius;
       this.sprite.added = true;
       this.sprite.cullable = true;
     }
   }
 
+  setPosition(x, y) {
+    this.x = x;
+    this.y = y;
+    this.sprite.x = x;
+    this.sprite.y = y;
+  }
+
   getRandomEdgeSpawnCoords(galaxy) {
     const camera = galaxy.camera;
-    const x0 = camera.x; // top-left x
-    const y0 = camera.y; // top-left y
+    const x0 = camera.x;
+    const y0 = camera.y;
     const w = camera.width;
     const h = camera.height;
 
     const perimeter = 2 * (w + h);
     const p = Math.random() * perimeter;
 
-    // Flatten the perimeter to a line, then wrap back to a point
     let x, y;
 
     if (p < w) {
-      // Top edge
       x = this.getPointOffset(x0 + p, 0);
       y = this.getPointOffset(y0, -1);
     } else if (p < w + h) {
-      // Right edge
       x = this.getPointOffset(x0 + w, 1);
       y = this.getPointOffset(y0 + (p - w), 0);
     } else if (p < 2 * w + h) {
-      // Bottom edge
       x = this.getPointOffset(x0 + (2 * w + h - p), 0);
       y = this.getPointOffset(y0 + h, 1);
     } else {
-      // Left edge
       x = this.getPointOffset(x0, -1);
       y = this.getPointOffset(y0 + (perimeter - p), 0);
     }
@@ -75,13 +79,6 @@ export class Enemy {
   getPointOffset(px, dir) {
     const spawnOffset = this.radius * 4;
     return px + dir * spawnOffset;
-  }
-
-  setPosition(x, y) {
-    this.x = x;
-    this.y = y;
-    this.sprite.x = x;
-    this.sprite.y = y;
   }
 
   destroy() {
